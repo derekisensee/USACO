870b21d886f9c3bda6c4c3d85365baf57ec08c3e
17isenseed <17isenseed@CTE0615810166>
2017-02-03 07:30:02 -0600

    Start Necklace

--- USACO/Necklace.cpp
+++ USACO/Necklace.cpp
@@ -4,8 +4,24 @@ PROG: beads
 LANG: C++
 */
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main() {
+string breakAtSpot(int spot, string n) {
+	return (n.substr(spot, n.length()) + n.substr(0, spot));
+}
 
+int main() {
+	ofstream out("beads.out");
+	ifstream in("beads.in");
+
+	int size; string necklace;
+
+	in >> size >> necklace;
+
+	cout << "Original: " << necklace << endl;
+	cout << "New: " << breakAtSpot(1, necklace);
+
+	return 0;
 }
